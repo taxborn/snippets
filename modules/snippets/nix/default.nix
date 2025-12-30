bfa2a65838a4499fecd4b5b2637cbc69ae30d743
Braxton Fair <hello@taxborn.com>
2025-12-30 11:15:58 -0600

    fix: syntax error

--- modules/snippets/nix/default.nix
+++ modules/snippets/nix/default.nix
@@ -1,16 +1,15 @@
 {lib, ...}: {
   options.mySnippets.nix.settings = lib.mkOption {
-      type = lib.types.attrs;
-      description = "Default nix settings shared across machines.";
+    type = lib.types.attrs;
+    description = "Default nix settings shared across machines.";
 
-      default = {
-        experimental-features = [
-          "flakes"
-          "nix-command"
-        ];
+    default = {
+      experimental-features = [
+        "flakes"
+        "nix-command"
+      ];
 
-        trusted-users = ["taxborn" "@admin" "@wheel"];
-      };
+      trusted-users = ["taxborn" "@admin" "@wheel"];
     };
   };
 }
