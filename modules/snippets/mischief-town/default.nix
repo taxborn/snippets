213a7c305ee2f5b1bbbe0a8bb6bb29a4deaf79ed
Braxton Fair <hello@taxborn.com>
2026-02-23 12:37:37 -0600

    feat(mischief-town): add tailscale IPs and internal service ports. (#2)
    Reviewed-on: https://git.mischief.town/nix-infra/snippets/pulls/2

--- modules/snippets/mischief-town/default.nix
+++ modules/snippets/mischief-town/default.nix
@@ -5,6 +5,17 @@
     description = "Hostnames, ports, and vHosts for mischief.town services.";
 
     default = {
+      tailscaleIPs = {
+        uranium = "100.64.0.0";
+        tungsten = "100.64.0.1";
+        "helium-01" = "100.64.1.0";
+        carbon = "100.64.2.0";
+      };
+
+      loki.port = 3100;
+      prometheus.port = 9090;
+      nodeExporter.port = 9100;
+
       forgejo = {
         hostname = "100.64.2.0";
         port = 8193;
